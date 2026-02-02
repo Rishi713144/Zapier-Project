54c8c65d4f669ace6c64d8c8e09899af2088e2fc
Rishi <116449371+Rishi713144@users.noreply.github.com>
2026-02-02 12:35:57 +0530

    first commit

---
+++ processor/prisma.config.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config_1 = require("@prisma/config");
+exports.default = (0, config_1.defineConfig)({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    },
+});
